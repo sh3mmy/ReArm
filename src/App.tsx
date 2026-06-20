a23915e4dba324ee5b5fd114e9d17b94f7a0ada5
sh3mmy <102486576+sh3mmy@users.noreply.github.com>
2026-06-20 09:59:46 +0100

    Updated index.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,34 @@
-// src/App.tsx
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-
-import Navigation from "./components/Navigation";
-
-// Pages
-import LandingPage from "./pages/LandingPage";
-import TheFuturePage from "./pages/TheFuturePage";
-import EngineeringPage from "./pages/EngineeringPage";
-import YourArmPage from "./pages/YourArmPage";
-import PrivateDemoPage from "./pages/PrivateDemoPage";
-import ProfilePage from "./pages/ProfilePage";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import LandingPage from './pages/LandingPage';
+import YourArmPage from './pages/YourArmPage';
+import EngineeringPage from './pages/EngineeringPage';
+import TheFuturePage from './pages/TheFuturePage';
+import PrivateDemoPage from './pages/PrivateDemoPage';
+import ProfilePage from './pages/ProfilePage';
+import { AuthProvider } from './context/AuthContext';
 
 const App: React.FC = () => {
   return (
-    <div className="min-h-screen flex flex-col bg-neutral-950 text-white">
-      {/* Ambient background glow */}
-      <div className="ambient-glow" />
-
-      {/* Top navigation */}
-      <Navigation />
-
-      {/* Main page content */}
-      <div className="flex-1">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/the-future" element={<TheFuturePage />} />
-          <Route path="/engineering" element={<EngineeringPage />} />
-          <Route path="/your-arm" element={<YourArmPage />} />
-          <Route path="/private-demo" element={<PrivateDemoPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-        </Routes>
-      </div>
-    </div>
+    <AuthProvider>
+      <BrowserRouter>
+        <div className="min-h-screen flex flex-col bg-neutral-950 text-white">
+          <div className="ambient-glow" />
+          <Navigation />
+          <main className="flex-1">
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/your-arm" element={<YourArmPage />} />
+              <Route path="/engineering" element={<EngineeringPage />} />
+              <Route path="/the-future" element={<TheFuturePage />} />
+              <Route path="/private-demo" element={<PrivateDemoPage />} />
+              <Route path="/profile" element={<ProfilePage />} />
+            </Routes>
+          </main>
+        </div>
+      </BrowserRouter>
+    </AuthProvider>
   );
 };
 
